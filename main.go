6b34eeea570bf94bda912f23c19c036b55fb91ae
Cookiery <2061803022@qq.com>
2023-08-29 19:38:45 +0800

    init

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.StaticFS("/api/v1/haojianl/img", http.Dir("./images"))
+	r.Run()
+}
